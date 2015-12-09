42d0931d9d481367828f555a1d38bee6e6522e2c
Antonio Leal <tonyjleal@gmail.com>
2015-12-09 23:59:36 +0100

    Task D: Cart Creation

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,17 @@
+class StoreController < ApplicationController
+
+  def index
+    @products = Product.order(:title)
+    @counter = increment_count
+  end
+
+  private
+
+  def increment_count
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
+  end
+
+end
